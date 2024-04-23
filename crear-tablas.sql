8e8d58f06868c6be1a16552233707c929a89d411
JulenXabierCano <JulenXabierCano>
2024-04-23 19:19:24 +0200

    prueba primer inicio

--- crear-tablas.sql
+++ crear-tablas.sql
@@ -3,24 +3,17 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `salas` (
-  `id_sala` int NOT NULL,
-  `jugador_1` varchar(255) NOT NULL,
-  `jugador_2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `movimiento_j1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `movimiento_j2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `mensaje_j1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `mensaje_j2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+  `id_sala` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `jugador_1` VARCHAR(255) NOT NULL,
+  `jugador_2` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `movimiento_j1` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `movimiento_j2` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mensaje_j1` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mensaje_j2` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-ALTER TABLE `salas`
-  ADD PRIMARY KEY (`id_sala`);
-
-ALTER TABLE `salas`
-  MODIFY `id_sala` int NOT NULL AUTO_INCREMENT;
-
-/*
 CREATE TABLE `oauth_clients` (
-    `user_id` int NOT NULL,
+    `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255),
     `secret` VARCHAR(255),
     `provider` VARCHAR(255),
@@ -28,27 +21,14 @@ CREATE TABLE `oauth_clients` (
     `personal_access_client` TINYINT(1),
     `password_client` TINYINT(1),
     `revoked` TINYINT(1),
-    `updated_at` TIMESTAMP,
-    `created_at` TIMESTAMP
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `created_at` TIMESTAMP DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `oauth_personal_access_clients` (
-    `client_id` int NOT NULL,
-    `updated_at` TIMESTAMP,
-    `created_at` TIMESTAMP
+    `client_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `created_at` TIMESTAMP DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-  
-ALTER TABLE `oauth_clients`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
-  
-ALTER TABLE `oauth_personal_access_clients`
-  MODIFY `client_id` int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `oauth_clients`
-  ADD PRIMARY KEY (`user_id`);
-
-ALTER TABLE `oauth_personal_access_clients`
-  ADD PRIMARY KEY (`client_id`);
-*/
 
 COMMIT;
